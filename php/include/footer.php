4c832c5f4b9489ccc9835eb3c4ee07154f771829
ojs104 <143478298+ojs104@users.noreply.github.com>
2023-10-01 22:56:49 +0900

    10-01
    퀴즈

---
+++ php/include/footer.php
@@ -0,0 +1,6 @@
+<footer id="footer" role="contentinfo">
+        <div class="footer__inner container btStyle">
+            <div>Copyright 2023 ojs104</div>
+            <div>blog by webs</div>
+        </div>
+    </footer>

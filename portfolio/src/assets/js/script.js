415fd2f014a5c9ea8a88540de1a6893dcf19db9b
ojs104 <kmdojs@gmail.com>
2023-12-04 14:12:28 +0900

    12-04

--- portfolio/src/assets/js/script.js
+++ portfolio/src/assets/js/script.js
@@ -19,15 +19,6 @@ $(function () {
 });
 
 
-
-// path 길이
-$(function () {
-    $('.svgAni').find('#svgAni05').each(function (i, path) {
-        var length = path.getTotalLength();
-        // alert(length);
-    });
-});
-
 /* slimplyScroll */
 $(function () {
     $(".con03 .list").simplyScroll({
@@ -37,6 +28,8 @@ $(function () {
     })
 })
 
+
+/* gsap ScrollTrigger */
 $(function () {
     gsap.registerPlugin(ScrollTrigger);
 
@@ -46,20 +39,21 @@ $(function () {
             start: 'top top',
             pin: true,
             pinSpacing: false,
-            markers:true 
-        })
-        
-    })
+            // markers: true
+        });
+    });
 
     ScrollTrigger.create({
-        snap: 1 / (section.length -1)
-    })
+        snap: 1 / (section.length - 1)
+    });
 })
+
+
 
 // a 속성 제거
 $(document).on('click', 'a[href="#"]', function (e) {
     e.preventDefault();
-    
+
 })
 
 // scrolla.js 적용
@@ -69,3 +63,78 @@ $(function () {
         once: false
     })
 });
+
+
+
+// 스크롤 하면서 sVG로 보여주는 라인 애니메이션
+function scrollifyPage() {
+    const $body = $('body');
+    const options = {
+        section: '.panel',
+        scrollSpeed: 1100,
+        scrollbars: false,
+        overflowScroll: false,
+        afterRender() {
+            $body.attr('data-pre-index', 0);
+        },
+
+        before(i, panels) {
+            $(panels[i]).addClass('active').
+                siblings().removeClass('active');
+
+            let preIndex = parseInt($body.attr('data-pre-index'));
+            let direction = i > preIndex ? 'down' : 'up';
+
+            $body.attr('data-pre-index', i).
+                removeClass('up down').
+                addClass(direction);
+
+            $('h1').text(direction);
+
+            $(document).trigger('onScrollify');
+        }
+    };
+
+
+    $.scrollify(options);
+}
+
+function drawSvg() {
+    const $panel = $('.panel');
+
+    function drawSVGPaths(_parentElement, _time) {
+        const paths = $(_parentElement).find('path');
+
+        paths.toArray().forEach(el => {
+            const $el = $(el);
+            const isUp = $('body').hasClass('up');
+            const totalLength = isUp ? -el.getTotalLength() : el.getTotalLength();
+
+            $el.css({
+                strokeDashoffset: totalLength,
+                strokeDasharray: `${Math.abs(totalLength)} ${Math.abs(totalLength)}`
+            });
+
+
+            $el.animate(
+                { strokeDashoffset: 0 },
+                { duration: _time });
+
+        });
+    }
+
+    function startSVGAnimation(parentElement) {
+        drawSVGPaths(parentElement, 1500);
+    }
+
+    function drawSvgInActivePanel() {
+        $panel.toArray().forEach(el => $(el).hasClass('active') && startSVGAnimation($(el).find('svg')));
+    }
+
+    drawSvgInActivePanel();
+
+    $(document).on('onScrollify', drawSvgInActivePanel);
+}
+
+scrollifyPage();
+drawSvg();
